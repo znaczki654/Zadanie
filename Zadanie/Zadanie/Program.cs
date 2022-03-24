9b6115cca1810ccf0fc9b235e26c046a213d8355
znaczki654 <znaczki654@wp.pl>
2022-03-24 13:23:51 +0100

    API Connection #02
    Made successfully the connection to the api.

--- Zadanie/Zadanie/Program.cs
+++ Zadanie/Zadanie/Program.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net.Http;
 
 namespace Zadanie {
     internal class Program {
         static void Main(string[] args) {
-            
+            APIConnect();
+        }
+
+
+
+        static void APIConnect() {
+            HttpClient client = new HttpClient();
+            var responseTask = client.GetAsync("https://catfact.ninja/fact");
+            responseTask.Wait();
+            if (responseTask.IsCompleted) {
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode) {
+                    var messageTask = result.Content.ReadAsStringAsync();
+                    messageTask.Wait();
+                    Console.WriteLine("Message from WebAPI: " + messageTask.Result);
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
